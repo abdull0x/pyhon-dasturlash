570671c1d924f3510ead874b2c3568ddf1edfc41
Abdulloh Akramov <@abdullohmirzayev1998>
2025-11-07 11:21:55 +0500

    ikkinchi zo'r dars

--- dars/abdullax.py
+++ dars/abdullax.py
@@ -7,3 +7,18 @@ for i in ism:
     letters.append(i)
     
 print("Natija: ",letters)
+
+
+
+# foydalanuvchi kiritgan takrorlanuchi va o'xshash harflarni 'chirib tashlovchi code
+
+
+
+lst = [1,2,2,3,3,4,4,5,5,]
+
+new_list = []
+
+for i in lst:
+    if i in new_list == False:
+        new_list.append(i)
+print("Natija: ",new_list)
